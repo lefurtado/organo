0edcabcb990a2f4a5936b43debbfec1e6129ec53
Leandro Furtado <le_furtado@outlook.com.br>
2023-07-05 22:50:09 -0300

    refat: 'Refatorado alguns nomes de props'

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -2,16 +2,16 @@ import Colaborador from '../Colaborador';
 import './Time.css';
 import hexToRgba from 'hex-to-rgba';
 
-const Time = ({ nome, cor, aoDeletarColaborador, colaboradores, mudarCorPrimaria  }) => {
+const Time = ({ time, nome, cor, aoDeletarColaborador, colaboradores, mudarCor }) => {
     return (
         colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/images/fundo.png)', backgroundColor: hexToRgba(cor, 0.6) }}>
             <h3 style={{ borderColor: cor }}>{nome}</h3>
-            <input onChange={e => mudarCorPrimaria(e.target.value, nome)} value={cor} className='input-cor-primaria' type="color" name="" id="" />
+            <input onChange={e => mudarCor(e.target.value, time.id)} value={cor} className='input-cor-primaria' type="color" name="" id="" />
             <div className='colaboradores'>
                 {colaboradores.map(colaborador => {
                     return (
                         <Colaborador 
-                            key={colaborador.nome}
+                            key={colaborador.id}
                             nome={colaborador.nome} 
                             cargo={colaborador.cargo} 
                             imagem={colaborador.imagem} 
