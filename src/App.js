efc3b4a7c5c036dd69589e6846e1a5b48acf4da7
Leandro Furtado <le_furtado@outlook.com.br>
2023-07-05 22:50:37 -0300

    update: 'Adicionado uuid nos times e nos colaboradores que serão adicionados aos times'

--- src/App.js
+++ src/App.js
@@ -3,35 +3,43 @@ import Banner from './components/Banner';
 import Formulario from './components/Formulario';
 import Time from './components/Time';
 import Rodape from './components/Rodape';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
   const [times, setTimes] = useState([
     {
+      id: uuidv4(),
       nome: 'Programação',
       cor: '#57C278',
     },
     {
+      id: uuidv4(),
       nome: 'Front-End',
       cor: '#82CFFA',
     },
     {
+      id: uuidv4(),
       nome: 'Data Science',
       cor: '#A6D157',
     },
     {
+      id: uuidv4(),
       nome: 'Devops',
       cor: '#E06B69',
     },
     {
+      id: uuidv4(),
       nome: 'UX e Design',
       cor: '#DB6EBF',
     },
     {
+      id: uuidv4(),
       nome: 'Mobile',
       cor: '#FFBA05',
     },
     {
+      id: uuidv4(),
       nome: 'Inovação e Gestão',
       cor: '#FF8A29',
     }
@@ -39,14 +47,21 @@ function App() {
 
   const [colaboradores, setColaboradores] = useState([]);
 
+  const idColaborador = uuidv4();
+
   const aoNovoColaboradorAdicionado = (colaborador) => {
+    setColaboradores([...colaboradores, colaborador=
+      {
+        id: idColaborador,
+        ...colaborador
+      }
+    ]);
     console.log(colaborador);
-    setColaboradores([...colaboradores, colaborador]);
   }
 
-  const mudarCorDoTimePrimaria = (cor, nome) => {
+  const mudarCorDoTime = (cor, id) => {
     setTimes(times.map(time => {
-      if(time.nome === nome) {
+      if(time.id === id) {
         time.cor = cor;
       }
       return time;
@@ -63,13 +78,15 @@ function App() {
       <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
       {times.map(time => 
           <Time 
-            key={time.nome} 
+            key={time.id} 
+            id={time.id}
             nome={time.nome} 
             cor={time.cor}
             corSecundaria={time.corSecundaria}
             colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
             aoDeletarColaborador={aoDeletar}
-            mudarCorPrimaria={mudarCorDoTimePrimaria}
+            mudarCor={mudarCorDoTime}
+            time={times}
           />
         )}
       <Rodape />
