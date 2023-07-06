e02b579795478bb77ba7de67eed83c6a18ce8629
Leandro Furtado <le_furtado@outlook.com.br>
2023-07-05 23:34:07 -0300

    bugfix: 'Corrigido alteração da cor de fundo'

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -2,21 +2,19 @@ import Colaborador from '../Colaborador';
 import './Time.css';
 import hexToRgba from 'hex-to-rgba';
 
-const Time = ({ time, nome, cor, aoDeletarColaborador, colaboradores, mudarCor }) => {
+const Time = ({ time, aoDeletar, colaboradores, mudarCor }) => {
     return (
-        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/images/fundo.png)', backgroundColor: hexToRgba(cor, 0.6) }}>
-            <h3 style={{ borderColor: cor }}>{nome}</h3>
-            <input onChange={e => mudarCor(e.target.value, time.id)} value={cor} className='input-cor-primaria' type="color" name="" id="" />
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/images/fundo.png)', backgroundColor: hexToRgba(time.cor, 0.6) }}>
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
+            <input onChange={e => mudarCor(e.target.value, time.id)} value={time.cor} className='input-cor-primaria' type="color" name="" id="" />
             <div className='colaboradores'>
                 {colaboradores.map(colaborador => {
                     return (
                         <Colaborador 
                             key={colaborador.id}
-                            nome={colaborador.nome} 
-                            cargo={colaborador.cargo} 
-                            imagem={colaborador.imagem} 
-                            corPrimaria={cor}
-                            aoDeletar={aoDeletarColaborador}
+                            corDeFundo={time.cor}
+                            aoDeletarColaborador={aoDeletar}
+                            colaborador={colaborador}
                         />
                     )
                 })}
